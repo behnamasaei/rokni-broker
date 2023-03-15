dc27e23ac73fd3a7980597a33e4724cb7c55ceac
BehnamAsaei <behnamasaei@gmail.com>
2023-03-15 09:58:38 +0330

    init menu

--- AppUI/src/app/app.component.ts
+++ AppUI/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AppUI';
+  menubar: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.menubar = [
+      {
+          label: 'تنظیمات',
+          items: [{
+                  label: 'صنایع', 
+                  routerLink:['/settings']
+              },
+              {label: 'Open'},
+              {label: 'Quit'}
+          ]
+      },
+      {
+          label: 'Edit',
+          icon: 'pi pi-fw pi-pencil',
+          items: [
+              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+          ]
+      }
+  ];
+  }
 }
