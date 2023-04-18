0a4d4bda35ed65c09cc6afaf1eadf5cf6365acb7
Behnam Asaei <behnamasaei@gmail.com>
2023-04-18 14:43:12 +0330

    implement scrap filter

---
+++ ScrapFilterTsetmc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Program;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    // Create a new ChromeDriver instance
+
+    using (var driver = new ChromeDriver())
+    {
+      driver.Manage().Window.Maximize();
+      driver.Navigate().GoToUrl("http://www.tsetmc.com/Loader.aspx?ParTree=15131F");
+      var doc = new HtmlDocument();
+
+      System.Console.WriteLine("Set your settings then press enter:");
+      Console.ReadKey();
+
+      doc.LoadHtml(driver.PageSource);
+      var node = doc.DocumentNode.SelectNodes("//*[@id='main']")
+                  .Select(x => x.ChildNodes.Select(x => x.ChildNodes[0].InnerText)).First().ToList();
+
+      using (TextWriter tw = new StreamWriter("./SavedList.txt"))
+      {
+        foreach (String s in node)
+          tw.WriteLine(s);
+      }
+    }
+
+
+
+  }
+}
